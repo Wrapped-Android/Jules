eb97f6f0eba80ac6f9876a57361f80d9e8d95a1d
Cleboost <clement.balarot@gmail.com>
2026-04-24 19:14:10 +0200

    ci: synchronize android versioning with github tags

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,9 @@ android {
         applicationId = "com.cleboost.jules"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        
+        versionCode = (project.findProperty("versionCode") as? String)?.toInt() ?: 1
+        versionName = (project.findProperty("versionName") as? String) ?: "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
